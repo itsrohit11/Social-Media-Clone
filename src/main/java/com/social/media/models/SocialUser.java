301315d23883da3443940a0e723365999e79824a
itsrohit11 <officialrohitjain11@gmail.com>
2025-02-04 18:38:22 +0530

    Different Relationships

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,5 +1,6 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,18 +19,23 @@ public class      SocialUser {
     private Long id;
 
     @OneToOne(mappedBy = "user")
-    //@JoinColumn(name = "social_profile_id")
+//    @JoinColumn(name = "social_profile_id")
+
     private SocialProfile socialProfile;
 
+
     @OneToMany(mappedBy = "socialUser")
+
     private List<Post> posts = new ArrayList<>();
 
     @ManyToMany
+
     @JoinTable(
             name = "user_group",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "group_id")
     )
+
     private Set<SocialGroup> groups = new HashSet<>();
 
     @Override
